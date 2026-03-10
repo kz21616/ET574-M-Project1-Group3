2602480a4f6a34291bf2d64555c63ecf16ba751c
kz21616 <kz21616@gmail.com>
2026-03-10 17:45:23 +0000

    issue found for loop not returning to menu

---
+++ project 1.py
@@ -0,0 +1,53 @@
+
+
+title = []
+time = []
+status = []
+
+def menu():
+    print("1. Add Book")
+    print("2. View reading list")
+    print("3. View summary")
+    print("4. Exit")
+  
+
+def add_book():
+   pass
+
+def reading_list():
+    pass
+
+def summary():
+    pass
+
+number = input("Enter choice: ")
+
+
+
+
+
+
+
+
+
+
+while True: 
+
+
+    menu()
+
+    if number == "1":
+        add_book()
+
+    elif number == "2":
+        reading_list()
+
+    elif number == "3":
+        summary()
+
+    elif number == "4":
+        print("Goodbye!")
+
+    else:
+        print("Invalid choice. Try again.")
+    
